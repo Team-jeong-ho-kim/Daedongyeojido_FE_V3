625441d041779e5f31dc6eb7c365f27ecff70778
minsu0707 <mandoo4137@gmail.com>
2024-12-06 01:19:14 +0900

    FIX : 프로젝트 소개 수정

--- src/pages/ClubBottomPages/IntroProject.jsx
+++ src/pages/ClubBottomPages/IntroProject.jsx
@@ -12,11 +12,14 @@ export const IntroProject = () => {
 
 const IntroProjectAll = styled.div`
   width: 100%;
-  padding: 0 0 0 17%;
-  margin-top: 70px;
+  padding: 7%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const IntroTitle = styled.h1`
   font-size: 34px;
   font-weight: 400;
+  margin-left: -62%;
 `;
