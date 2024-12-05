d806b0abbabe683a63c0c3727877d95afb80e85d
김 찬 <gimchan29@naver.com>
2024-12-05 22:49:57 +0900

    FIX : 모집중버튼 컴포넌트 수정

--- src/components/SeeallPage/CollectBtn.jsx
+++ src/components/SeeallPage/CollectBtn.jsx
@@ -5,7 +5,7 @@ export const CollectBtn = () => {
   return (
     <Container>
       <Badge>
-        모집중
+        모집 중
       </Badge>
     </Container>
   );
@@ -13,21 +13,23 @@ export const CollectBtn = () => {
 
 const Container = styled.div`
   display: flex;
-  justify-content: flex-end; /* 수평 정렬: 오른쪽 */
+  justify-content: flex-end;
   align-items: flex-end;
-  width: 100%; /* 부모 요소 크기에 따라 조정 */
+  width: 100%;
   height: 100%; 
 `;
 
 const Badge = styled.span`
   padding: 5px 10px;
-  background-color: red;
-  color: white;
-  border-radius: 4px;
-  font-size: 12px;
+  background-color: #B9B9B9;
+  color: #000000;
+  border-radius: 12px;
+  font-size: 14px;
   display: flex;
-  justify-content: center; /* 내부 텍스트 중앙 정렬 */
-  align-items: center; /* 내부 텍스트 중앙 정렬 */
-  width: 52px;
+  justify-content: center;
+  align-items: center;
+  width: 60px;
+  margin: 0px 5px 5px 0px;
+
 `;
 
