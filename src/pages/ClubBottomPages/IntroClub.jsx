6f433f58d3cf080626747bb7084bdb7ae4c6be18
김 찬 <gimchan29@naver.com>
2024-12-06 01:29:51 +0900

    FEAT : IntroClub페이지 퍼블리싱

--- src/pages/ClubBottomPages/IntroClub.jsx
+++ src/pages/ClubBottomPages/IntroClub.jsx
@@ -3,21 +3,27 @@ import { ExClub } from "../../components/Club/ExClub";
 
 export const IntroClub = () => {
   return (
+    <Container>
     <IntroProjectAll>
       <IntroTitle>동아리 소개</IntroTitle>
       <ExClub />
     </IntroProjectAll>
+    </Container>
   );
 };
 
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
 const IntroProjectAll = styled.div`
-  width: 100%;
-  padding: 0 0 0 17%;
+  width: 1000px;
   margin-top: 70px;
+  border: 1px solid black;
 `;
 
 const IntroTitle = styled.h1`
   font-size: 34px;
   font-weight: 400;
 `;
-
