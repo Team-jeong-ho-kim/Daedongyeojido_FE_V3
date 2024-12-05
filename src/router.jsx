8ed0620e0c5419336bcd9c2df66b0dfd79034de1
minsu0707 <mandoo4137@gmail.com>
2024-12-05 18:41:17 +0900

    Revert "CHORE : 유저, 어드민 폴더 세팅"
    This reverts commit 6149993af367df362924adad39f7bde8479f2775.

--- src/router.jsx
+++ src/router.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import styled from "styled-components";
 
 import { SeeAll } from "./pages/SeeAllPage";
-import { ClubMain } from "./pages/user/ClubMain";
+import { ClubMain } from "./pages/ClubMain";
 
 export const Router = () => {
   return (
