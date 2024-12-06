e3601093dc938ddd3c823900b3a860e51a14db2a
김 찬 <gimchan29@naver.com>
2024-12-06 09:51:39 +0900

    FEAT : Default페이지 추가

---
+++ src/pages/ClubBottomPages/NullClub.jsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export const NullClub = () => {
+  return (
+    <>
+    <Container>
+    <IntroProjectAll>
+      <NullTitle>아직 등록된 것이 없습니다</NullTitle>
+      <SubNullIntro>나중에 다시 시도해보시기 바랍니다</SubNullIntro>
+    </IntroProjectAll>
+    </Container>
+    <Blank />
+    </>
+  );
+};
+
+const Blank = styled.div`
+  height: 50px;
+`
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+`
+
+const IntroProjectAll = styled.div`
+  margin-top: 150px;
+`;
+
+const NullTitle = styled.p`
+  font-size: 34px;
+  font-weight: 400;
+  color: #B2B2B2;
+`;
+
+const SubNullIntro = styled.p`
+  color: #B2B2B2;
+  margin-top: 10px;
+`
