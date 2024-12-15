ce9455909df8a81f5c546af214056c92c7bed810
김 찬 <changim104@gmail.com>
2024-12-15 22:57:57 +0900

    FIX : 지원서 페이지 라우터 수정

--- src/router.jsx
+++ src/router.jsx
@@ -3,12 +3,14 @@ import styled from "styled-components";
 
 import { SeeAll } from "./pages/SeeAllPage";
 import { ClubMain } from "./pages/ClubMain";
+import { ApplicationForm } from"./pages/ApplicationForm";
 
 export const Router = () => {
   return (
     <Container>
       <BrowserRouter>
         <Routes>
+          <Route path="/Apply" element={<ApplicationForm />}/>
           <Route path="/seeall" element={<SeeAll />} />
           <Route path="/club" element={<ClubMain />} />
         </Routes>
