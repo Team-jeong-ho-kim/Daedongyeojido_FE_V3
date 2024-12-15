caa74d64347cc49e71125bbb1cd0a6c47a94d86c
김 찬 <changim104@gmail.com>
2024-12-15 22:57:25 +0900

    feat : 지원서 양식 작성

--- src/pages/ApplicationForm.jsx
+++ src/pages/ApplicationForm.jsx
@@ -1,74 +1,91 @@
 import React from "react";
 import styled from "styled-components";
+import { Header } from "../components/Header";
 
 export const ApplicationForm = () => {
   return (
-    
+    <>
+      <Header />
     <Container>
+      <Back> &lt; 뒤로가기</Back>
       <Title>지원서 작성</Title>
+            <SubmitButton>제출하기</SubmitButton>
       <SubTitle>
-      동아리에서 당신에 대해 궁금한 것이 많습니다. <br />
-      아래 문항들에 답변하여 자기가 어떤 사람인지 알려주세요.
+        동아리에서 당신에 대해 궁금한 것이 많습니다. <br />
+        아래 문항들에 답변하여 자기가 어떤 사람인지 알려주세요.
       </SubTitle>
 
-    <FormContainer>
-      <Section> 
-        <TitleContainer>
-        <Name>김미영</Name>
-        <StudentNumber>1303</StudentNumber>
-        </TitleContainer>
-      </Section>
+      <FormContainer>
+        <Section>
+          <TitleContainer>
+            <Name>김미영</Name>
+            <StudentNumber>1303</StudentNumber>
+          </TitleContainer>
+        </Section>
 
-      <Section>
-        <RadioSection>
-        <Label>모집 분야</Label>
-        <RadioGroup>
-          {[
-            "Back-End",
-            "Front-End",
-            "Game Developer",
-            "Embedded",
-            "AI",
-          ].map((field) => (
-            <RadioLabel key={field}>
-              <RadioButton type="radio" name="field" value={field} />
-              {field}
-            </RadioLabel>
-          ))}
-        </RadioGroup>
-        </RadioSection>
-      </Section>
+        <Section>
+          <RadioSection>
+            <Label>모집 분야</Label>
+            <RadioGroup>
+              {[
+                "Back-End",
+                "Front-End",
+                "Game Developer",
+                "Embedded",
+                "AI",
+                "IOS",
+                "AOS",
+                "Fluter",
+                "Devops",
+                "Security",
+                "Design"
+              ].map((field) => (
+                <Radio>
+                <RadioLabel key={field}>
+                  <RadioButton type="radio" name="field" value={field} />
+                  {field}
+                </RadioLabel>
+                </Radio>
+              ))}
+            </RadioGroup>
+          </RadioSection>
+        </Section>
 
-      <Section>
-        <Label>자기소개</Label>
-        <TextArea placeholder="자기소개를 작성해 주세요" />
-      </Section>
+        <Section>
+          <Label>자기소개</Label>
+          <TextArea placeholder="자기소개를 작성해 주세요" />
+        </Section>
 
-      <Section>
-        <Label>지원하게 된 계기가 무엇인가요?</Label>
-        <TextArea placeholder="지원 계기를 작성해 주세요" />
-      </Section>
+        <Section>
+          <Label>지원하게 된 계기가 무엇인가요?</Label>
+          <TextArea placeholder="지원 계기를 작성해 주세요" />
+        </Section>
 
-      <Section>
-        <Label>본인의 전공을 선택한 이유가 무엇인가요?</Label>
-        <TextArea placeholder="전공 선택 계기를 작성해 주세요" />
-      </Section>
+        <Section>
+          <Label>본인의 전공을 선택한 이유가 무엇인가요?</Label>
+          <TextArea placeholder="전공을 선택한 계기를 작성해 주세요" />
+        </Section>
 
-      <Section>
-        <Label>동아리에게 바라는 점</Label>
-        <TextArea placeholder="동아리에게 바라는 점을 작성해 주세요" />
-      </Section>
+        <Section>
+          <Label>동아리에게 바라는 점</Label>
+          <TextArea placeholder="동아리에게 바라는 점을 작성해 주세요" />
+        </Section>
 
-      <Section>
-        <Label>과제</Label>
-        <TextArea placeholder="과제 내용을 작성해 주세요" />
-      </Section>
+        <Section>
+          <Label>과제</Label>
+          <TextArea placeholder="과제 과제 링크 첨부, 과제 내용 작성 등 동아리에서 요구한 방법에 맞춰 작성해 주세요 작성해 주세요" />
+        </Section>
 
-      <SubmitButton>제출하기</SubmitButton>
-    </FormContainer>
+      </FormContainer>
     </Container>
+</>
   );
 };
+
+const Radio = styled.div`
+  display: flex;
+  justify-content: flex-start;
+`
 
 const Container = styled.div`
   font-family: "Pretendard";
@@ -76,21 +93,25 @@ const Container = styled.div`
   max-width: 768px;
   margin: 0 auto;
   padding: 20px;
-  font-family: Arial, sans-serif;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-  border: 1px solid black;
   user-select: none; 
   -webkit-user-select: none;
 `;
 
 const FormContainer = styled.div`
-  font-family: "Pretendard";
-  padding: 35px 35px 35px 35px;
-  border: 1px solid black;
+  padding: 35px;
+  border: 1px solid #B2B2B2;
+`;
+
+const Back = styled.p`
+  display: flex;
+  justify-content: flex-start;
+  font-size: 12px;
+  margin-bottom: 20px;
 `
+
 const TitleContainer = styled.div`
   display: flex;
-`
+`;
 
 const Title = styled.h1`
   font-size: 28px;
@@ -102,14 +123,12 @@ const Title = styled.h1`
 
 const SubTitle = styled.p`
   font-size: 12px;
-  margin-bottom: 20px;
+  margin-bottom: 40px;
   font-family: "Pretendard";
   justify-content: flex-end;
-`
+`;
 
-const Name = styled.h1`
-  
-`
+const Name = styled.h1``;
 
 const StudentNumber = styled.p`
   display: flex;
@@ -117,7 +136,7 @@ const StudentNumber = styled.p`
   flex-direction: column;
   margin-left: 10px;
   font-size: 20px;
-`
+`;
 
 const Section = styled.div`
   margin-bottom: 20px;
@@ -128,14 +147,6 @@ const Label = styled.label`
   font-size: 20px;
   font-weight: bold;
   margin-bottom: 8px;
-`;
-
-const Input = styled.input`
-  width: 100%;
-  padding: 10px;
-  font-size: 14px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
 `;
 
 const TextArea = styled.textarea`
@@ -165,23 +176,22 @@ const TextArea = styled.textarea`
   }
 `;
 
-
 const RadioSection = styled.div`
-  height: 200px;
-  border: 1px solid #ccc;
-  overflow: hidden;
-`
-const RadioGroup = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
+  height: auto;
 `;
 
-const RadioLabel = styled.label`
+const RadioGroup = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+  margin-top: 20px;
+`;
+
+const RadioLabel = styled.div`
   display: flex;
   align-items: center;
   font-size: 18px;
-  margin-left: 5px;
+  justify-content: center;
 `;
 
 const RadioButton = styled.input`
@@ -194,15 +204,15 @@ const RadioButton = styled.input`
 `;
 
 const SubmitButton = styled.button`
-  width: 100%;
-  padding: 12px;
-  font-size: 16px;
+  padding: 10px 55px 10px 55px;
+  font-size: 13px;
   color: #fff;
-  background-color: #007bff;
+  background-color: #52565D;
   border: none;
   border-radius: 4px;
   cursor: pointer;
   text-align: center;
+  float: right; 
 
   &:hover {
     background-color: #0056b3;
