286bdf10c393054b344c97ec32b09913eb085b48
김 찬 <changim104@gmail.com>
2024-12-16 19:04:14 +0900

    fix : Introteammate컴포넌트 수정

--- user/src/components/Club/IntroTeammate.jsx
+++ user/src/components/Club/IntroTeammate.jsx
@@ -1,62 +1,35 @@
 import styled from "styled-components";
-import Round from "../../assets/Exround.svg";
+import { Team } from "../Club/Team";
 
 export const Teammate = () => {
   return (
-    <ClubAll>
-      <ClubInfo>
-        <TeamInfo_3>
-          <CardTitle>
-            <ExRound src={Round} />
-            박주원
-          <Major>Backend</Major>
-          </CardTitle>
-          <CardSub>
-            디자인 할래요
-          </CardSub>
-        </TeamInfo_3>
-      </ClubInfo>
-    </ClubAll>
+    <Container>
+    <TeamAll>
+      <Team></Team>
+      <Team></Team>
+      <Team></Team>
+      <Team></Team>
+      <Team></Team>
+      <Team></Team>
+    </TeamAll>
+    </Container>
   );
 };
 
-
-const ClubAll = styled.div`
-  width: 1200px;
-  margin-top: 3%;
-  border-radius: 7px;
-  overflow: hidden;
+const Container = styled.div`
   display: flex;
-`;
-
-const ClubInfo = styled.p`
-  margin: 0;
-  font-size: 20px;
-  color: #5E5E5E;
+  justify-content: center;
 `
-const TeamInfo_3 = styled.div`
+
+const TeamAll = styled.div`
+  height: 100%;
+  margin-top: 20px;
+  margin-bottom: 50px;
+  border-radius: 7px;
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 20px 20px 20px 20px;
-  border: 1px solid black;
-`;
-
-const CardTitle = styled.p`
-  color: black;
-  display: flex;
-  gap: 10px;
-  align-items: flex-end;
-`;
-
-const CardSub = styled.p`
-  
-`;
-
-const Major = styled.p`
-  
-`;
-
-const ExRound = styled.img`
-  
+  padding: 10px 10px 10px 10px;
+  gap: 20px;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  margin-left : 80px;
 `;
