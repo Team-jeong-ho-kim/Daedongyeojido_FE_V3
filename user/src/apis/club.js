283440ff8af86cc34fba60d8cebd568d09db2057
은지 <wej0513@dsm.hs.kr>
2024-12-16 09:02:03 +0900

    chore: club api 세팅

---
+++ user/src/apis/club.js
@@ -0,0 +1,17 @@
+import { instance } from "./axios";
+
+const router = "/club";
+
+export const getClub = async () => {
+  const response = await instance.get(`${router}/all`);
+  return response.data;
+};
+
+export const getClubDetail = async (clubname) => {
+  const response = await instance.get(`${router}/info/${clubname}`);
+  return response.data;
+};
+
+export const modifyClubInfo = async (data) => {
+  return await instance.patch(`${router}/modify`, data);
+};
